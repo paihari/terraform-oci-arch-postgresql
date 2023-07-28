4e4563153d2c23d83c060a88c4aabfe15afb8e85
paihari <hpai.bantwal@gmail.com>
2023-07-28 12:30:39 +0200

    Setup windmill db

--- scripts/postgresql_master_setup.sql
+++ scripts/postgresql_master_setup.sql
@@ -1,3 +1,10 @@
 ALTER SYSTEM SET listen_addresses TO '*';
 CREATE USER ${pg_replicat_username} REPLICATION LOGIN ENCRYPTED PASSWORD '${pg_replicat_password}';
 
+CREATE DATABASE windmill;
+
+CREATE USER windmill WITH PASSWORD '${pg_replicat_password}';
+GRANT ALL PRIVILEGES ON DATABASE windmill TO windmill;
+ALTER ROLE windmill WITH SUPERUSER;
+
+
